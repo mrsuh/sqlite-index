df6f4aa2313c9aedcc38c2a1819b8416073ab601
Anton Sukhachev <mrsuh6@gmail.com>
2024-11-10 10:05:40 +0200

    Fixed typo

--- src/Parser/Search.php
+++ src/Parser/Search.php
@@ -132,13 +132,12 @@ class Search
         $count = ['search' => 0, 'filter' => 0, 'other' => 0];
         $key = 'other';
         $started = false;
-        $finished = false;
         foreach ($lines as $line) {
             if (str_starts_with($line, 'sqlite3DebugBtreeIndexMoveto: key')) {
-                if ($started && !$finished) {
-                    $count['search'] = 0;
-                    $count['filter'] = 0;
-                }
+
+                $count['search'] = 0;
+                $count['filter'] = 0;
+                
                 $started = true;
                 $key = 'search';
                 continue;
@@ -158,7 +157,6 @@ class Search
 
             if (str_starts_with($line, 'sqlite3DebugResultRow:')) {
                 $key = 'filter';
-                $finished = true;
             }
         }
 
